1c28212f8e7779fcc5626cab8665037f1aeb8b0f
kkigomi <kkigomi@duck.com>
2024-05-10 02:07:18 +0900

    feat: alert 메시지에서 소셜로그인을 할 수 있도록 개선

--- plugin/nariya/bbs/alert.php
+++ plugin/nariya/bbs/alert.php
@@ -1,12 +1,14 @@
 <?php
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+if (!defined('_GNUBOARD_')) {
+    exit;
+}
 
-if($error) {
+if ($error) {
     $g5['title'] = "오류안내 페이지";
 } else {
     $g5['title'] = "결과안내 페이지";
 }
-include_once(G5_PATH.'/head.sub.php');
+include_once (G5_PATH . '/head.sub.php');
 // 필수 입력입니다.
 // 양쪽 공백 없애기
 // 필수 (선택 혹은 입력)입니다.
@@ -29,84 +31,59 @@ $msg = isset($msg) ? strip_tags($msg) : '';
 $msg2 = str_replace("\\n", "<br>", $msg);
 
 $url = isset($url) ? clean_xss_tags($url, 1) : '';
-if (!$url) $url = isset($_SERVER['HTTP_REFERER']) ? clean_xss_tags($_SERVER['HTTP_REFERER'], 1) : '';
+if (!$url) {
+    $url = isset($_SERVER['HTTP_REFERER']) ? clean_xss_tags($_SERVER['HTTP_REFERER'], 1) : '';
+}
 
 $url = preg_replace("/[\<\>\'\"\\\'\\\"\(\)]/", "", $url);
-$url = preg_replace('/\r\n|\r|\n|[^\x20-\x7e]/','', $url);
+$url = preg_replace('/\r\n|\r|\n|[^\x20-\x7e]/', '', $url);
 
 // url 체크
 check_url_host($url, $msg);
 
-if($error) {
+if ($error) {
     $header2 = "다음 항목에 오류가 있습니다.";
 } else {
     $header2 = "다음 내용을 확인해 주세요.";
 }
+
+if (!$url) {
+    $url = 'javascript: history.back();';
+}
+
+if (!$member['mb_id']) {
+    $url = \G5_URL;
+}
 ?>
 
+<div class="modal modal-dialog modal-dialog-centered" id="alert-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h4><?= $msg2 ?></h4>
+                <?php if (!$member['mb_id']) { ?>
+                    <?php
+                    $urlencode = $GLOBALS['urlencode'];
+                    @include_once (get_social_skin_path() . '/social_login.offcanvas.php');
+                    ?>
+                <?php } ?>
+            </div>
+            <div class="modal-footer">
+                <a href="<?= $url ?>" class="btn btn-primary">돌아가기</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
-na_alert("<?php echo $msg2 ?>", function(){
-	<?php if ($url) { ?>
-	document.location.replace("<?php echo str_replace('&amp;', '&', $url); ?>");
-	<?php } else { ?>
-	history.back();
-	<?php } ?>
-});
+    const alertModal = new bootstrap.Modal('#alert-modal', {});
+    alertModal.show();
 </script>
 
-<noscript>
-<div id="validation_check">
-    <h1><?php echo $header2 ?></h1>
-    <p class="cbg">
-        <?php echo $msg2 ?>
-    </p>
-    <?php if($post) { ?>
-    <form method="post" action="<?php echo $url ?>">
-    <?php
-    foreach($_POST as $key => $value) {
-        
-        $key = clean_xss_tags($key);
-        $value = clean_xss_tags($value);
-
-        if(strlen($value) < 1)
-            continue;
-
-        if(preg_match("/pass|pwd|capt|url/", $key))
-            continue;
-    ?>
-    <input type="hidden" name="<?php echo htmlspecialchars($key); ?>" value="<?php echo htmlspecialchars($value); ?>">
-    <?php
+<style>
+    .modal-backdrop {
+        --bs-backdrop-opacity: 0.8 !important;
     }
-    ?>
-    <input type="submit" value="돌아가기">
-    </form>
-    <?php } else { ?>
-    <div class="btn_confirm">
-        <a href="<?php echo $url ?>">돌아가기</a>
-    </div>
-    <?php } ?>
-
-<?php /*
-<article id="validation_check">
-<header>
-    <hgroup>
-        <!-- <h1>회원가입 정보 입력 확인</h1> --> <!-- 수행 중이던 작업 내용 -->
-        <h1><?php echo $header ?></h1> <!-- 수행 중이던 작업 내용 -->
-        <h2><?php echo $header2 ?></h2>
-    </hgroup>
-</header>
-<p>
-    <!-- <strong>항목</strong> 오류내역 -->
-    <!--
-    <strong>이름</strong> 필수 입력입니다. 한글만 입력할 수 있습니다.<br>
-    <strong>이메일</strong> 올바르게 입력하지 않았습니다.<br>
-    -->
-    <?php echo $msg2 ?>
-</p>
-<a href="<?php echo $url ?>">돌아가기</a>
-</article>
-*/ ?>
-</div>
-</noscript>
+</style>
 <?php
-include_once(G5_PATH.'/tail.sub.php');
+include_once (G5_PATH . '/tail.sub.php');
