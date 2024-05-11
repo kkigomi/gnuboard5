ed7fc9feacae9cb029a7c04bd3c40b8f89f953fa
김선도 <98006861+damoang-net@users.noreply.github.com>
2024-05-12 03:48:12 +0900

    member

--- plugin/nariya/comment.skin.php
+++ plugin/nariya/comment.skin.php
@@ -141,6 +141,7 @@ if ($is_admin) {
 $comment_action_url = https_url(G5_BBS_DIR)."/write_comment_update.php";
 $comment_common_url = short_url_clean(G5_BBS_URL.'/board.php?'.clean_query_string($_SERVER['QUERY_STRING']));
 
+$list = run_replace('view_comment_list', $list);
 include_once($board_skin_path.'/view_comment.skin.php');
 
 if (!$member['mb_id']) // 비회원일 경우에만
