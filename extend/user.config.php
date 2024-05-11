5491cfd94da0e78272c809428f780f4b8202e452
김선도 <98006861+damoang-net@users.noreply.github.com>
2024-05-11 14:30:49 +0900

    test

--- extend/user.config.php
+++ extend/user.config.php
@@ -1,3 +1,5 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-if(preg_match("/.*\/bbs\/(register|register_form|register_member|member_leave)\.php?/", $_SERVER['REQUEST_URI'])) die('<script>confirm("당분간 눈으로만 응원해 주세요.");location.replace("'.G5_URL.'"); </script>');
+if (preg_match("/.*\/(bbs\/(register|register_form|register_member|member_leave)|plugin\/social\/register_member)\.php/", $_SERVER['REQUEST_URI'])) {
+    die('<script>confirm("당분간 눈으로만 응원해 주세요.");location.replace("'.G5_URL.'"); </script>');
+}
