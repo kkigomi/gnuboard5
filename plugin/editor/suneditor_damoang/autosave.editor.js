346a8b7c1a704df1ad0842a37bb18c21e68d4120
김선도 <98006861+damoang-net@users.noreply.github.com>
2024-05-11 14:43:04 +0900

    submodule 변경

--- plugin/editor/suneditor_damoang/autosave.editor.js
+++
@@ -1,8 +0,0 @@
-function get_editor_wr_content() {
-    return document.suneditor.getContents();
-}
-
-function put_editor_wr_content(content) {
-    document.suneditor.setContents(content);
-    return;
-}
