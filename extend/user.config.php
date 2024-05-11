c2fe857be785fcff30749bbf09918891ea806a93
김선도 <98006861+damoang-net@users.noreply.github.com>
2024-05-11 14:23:53 +0900

    test

--- extend/user.config.php
+++ extend/user.config.php
@@ -1,6 +1,3 @@
 <?php
-define('IS_YC', true); // 또는 false
-define('IS_DEMO', true); // 또는 false
-
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 if(preg_match("/.*\/bbs\/(register|register_form|member_leave)\.php?/", $_SERVER['REQUEST_URI'])) die('<script>confirm("회원가입 하실 필요 없습니다.");location.replace("'.G5_URL.'"); </script>');
