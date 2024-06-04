e07e5ca5bd44ee258a52a14c1f8daaf931655659
kkigomi <kkigomi@duck.com>
2024-06-05 01:27:55 +0900

    chore: redis cache 코드 비활성

--- lib/damoang/bootstrap.php
+++ lib/damoang/bootstrap.php
@@ -2,37 +2,37 @@
 
 declare(strict_types=1);
 
-use Damoang\Lib\Cache\RedisCache;
+// use Damoang\Lib\Cache\RedisCache;
 
 // Redis Cache
-add_replace('get_cachemanage_instance', function () {
-    static $instance = null;
+// add_replace('get_cachemanage_instance', function () {
+//     static $instance = null;
 
-    if (
-        ($_ENV['CACHE_REDIS_USE'] ?? 'false') === 'true' && $instance === null
-    ) {
-        $config = [
-            'host' => $_ENV['CACHE_REDIS_HOST'] ?? '127.0.0.1',
-            'port' => intval($_ENV['CACHE_REDIS_PORT'] ?? 6379),
-            'timeout' => intval($_ENV['CACHE_REDIS_TIMEOUT'] ?? 0)
-        ];
+//     if (
+//         ($_ENV['CACHE_REDIS_USE'] ?? 'false') === 'true' && $instance === null
+//     ) {
+//         $config = [
+//             'host' => $_ENV['CACHE_REDIS_HOST'] ?? '127.0.0.1',
+//             'port' => intval($_ENV['CACHE_REDIS_PORT'] ?? 6379),
+//             'timeout' => intval($_ENV['CACHE_REDIS_TIMEOUT'] ?? 0)
+//         ];
 
-        try {
-            if (!class_exists('Redis', false)) {
-                throw new \Exception('Class Redis not found');
-            }
+//         try {
+//             if (!class_exists('Redis', false)) {
+//                 throw new \Exception('Class Redis not found');
+//             }
 
-            $instance = new RedisCache($config);
-        } catch (\Exception $e) {
-            $instance = null;
-            if ($GLOBALS['is_admin'] === 'super') {
-                var_dump(
-                    '관리자용 오류 메시지 : Redis 오류',
-                    $e->getMessage()
-                );
-            }
-        }
-    }
+//             $instance = new RedisCache($config);
+//         } catch (\Exception $e) {
+//             $instance = null;
+//             if ($GLOBALS['is_admin'] === 'super') {
+//                 var_dump(
+//                     '관리자용 오류 메시지 : Redis 오류',
+//                     $e->getMessage()
+//                 );
+//             }
+//         }
+//     }
 
-    return $instance;
-});
+//     return $instance;
+// });
