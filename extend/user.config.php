46ec9d6445ea2a3e8fe326b9c8951ca723da3ea9
sdk <sdk@sdk.xyz>
2024-06-07 09:54:08 +0900

    no ang

--- extend/user.config.php
+++ extend/user.config.php
@@ -1,5 +1,5 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 if (preg_match("/.*\/(plugin\/social\/register_member)\.php/", $_SERVER['REQUEST_URI'])) {
-    die('<script>confirm("당분간 눈으로만 응원해 주세요.");location.replace("'.G5_URL.'"); </script>');
+    die('<script>confirm("실명 인증 도입 작업중입니다.");location.replace("'.G5_URL.'"); </script>');
 }
