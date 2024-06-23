ae326eba1c9330c715913ec6cc51c3f89f937bda
sdk <sdk@sdk.xyz>
2024-06-23 10:23:15 +0900

    앙티콘 기본 150

--- theme/damoang/app/clip.emo.php
+++ theme/damoang/app/clip.emo.php
@@ -112,7 +112,7 @@ function unFocus(id) {
     }
 }
 function clip_insert(txt) {
-    var clip = "{emo:" + txt + ":100}";
+    var clip = "{emo:" + txt + ":77}";
 
     if(parent.document.suneditor) {
         parent.document.suneditor.insertHTML(clip);
