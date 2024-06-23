b94d507c452d8571f7e28148d8b0a5cb34b40395
sdk <sdk@sdk.xyz>
2024-06-23 12:45:33 +0900

    해외 앙님 실명인증

--- theme/damoang/layout/basic/component/sidebar.php
+++ theme/damoang/layout/basic/component/sidebar.php
@@ -13,6 +13,12 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 <div class="na-menu">
     <div class="nav nav-pills nav-vertical">
+        <div class="nav-item">
+            <a class="nav-link" href="/verification" data-placement="left">
+                <!--                        <i class="bi-youtube nav-icon"></i>-->
+                <span class="nav-link-title">해외앙님 실명인증</span>
+            </a>
+        </div>
         <div id="sidebar-site-menu" class="mb-3">
             <?php
             if (!empty($config['cf_9'])) {
