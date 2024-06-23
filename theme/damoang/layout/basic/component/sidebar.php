9e58d950555dca54761f5908a729e7bf3af9cc28
sdk <sdk@sdk.xyz>
2024-06-23 18:03:04 +0900

    디스코드 입단 신청

--- theme/damoang/layout/basic/component/sidebar.php
+++ theme/damoang/layout/basic/component/sidebar.php
@@ -149,16 +149,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
                     <?php echo na_widget('damoang-image-banner', 'side-banner'); ?>
                 </div>
 
-                <script async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6922133409882969"
-                    crossorigin="anonymous"></script>
-                <!-- sub -->
-                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6922133409882969"
-                    data-ad-slot="3231235128" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-
+                애드센스 제거 후 파랑색 검색 바가 제거 되는 지 TEST
                 <!-- 배너 -->
                 <div class="d-none d-lg-block justify-content-center my-4">
                     <?php echo na_widget('damoang-image-banner', 'side-banner'); ?>
