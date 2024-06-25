f48033f7615156eaca71c2bf5a6d42372ae51233
kkigomi <kkigomi@duck.com>
2024-06-25 19:48:55 +0900

    chore: 사고방지를 위한 비활성 설정 추가

--- lib/damoang/bootstrap-installed.php
+++ lib/damoang/bootstrap-installed.php
@@ -46,13 +46,15 @@ add_replace('get_cachemanage_instance', function () {
 });
 
 // 회원 객체 반환
-// add_replace('get_member', function ($data = null, $mb_id, $fields, $is_cache) {
-//     if (!is_array($data) || $data instanceof Damoang\Lib\G5\Member\Member) {
-//         return $data;
-//     }
+if (!(($_ENV['DISABLE_MEMBER_OBJECT'] ?? 'false') === 'true')) {
+    add_replace('get_member', function ($data = null, $mb_id, $fields, $is_cache) {
+        if (!is_array($data) || $data instanceof Damoang\Lib\G5\Member\Member) {
+            return $data;
+        }
 
-//     return new Damoang\Lib\G5\Member\Member($data);
-// }, \G5_HOOK_DEFAULT_PRIORITY, 4);
+        return new Damoang\Lib\G5\Member\Member($data);
+    }, \G5_HOOK_DEFAULT_PRIORITY, 4);
+}
 
 // 다중이
 add_event('console:register', function ($application) {
