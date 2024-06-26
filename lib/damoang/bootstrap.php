fddd0ea77284998c92216cb29395484046e8a04b
kkigomi <kkigomi@duck.com>
2024-06-26 18:38:11 +0900

    fix: #155 댓글 쿼리의 인덱스 추가 보완

--- lib/damoang/bootstrap.php
+++ lib/damoang/bootstrap.php
@@ -4,3 +4,22 @@ declare(strict_types=1);
 if (!defined('_GNUBOARD_')) {
     exit;
 }
+
+// 댓글 페이징 인덱스 추가
+add_replace('admin_dbupgrade', function ($is_check = false) {
+    global $g5;
+
+    $result = sql_query("SELECT bo_table FROM `{$g5['board_table']}`");
+
+    while ($table = sql_fetch_array($result)) {
+        $tableName = get_write_table_name($table['bo_table']);
+
+        $resultIndex = sql_fetch("SHOW INDEX FROM `{$tableName}` where `Key_name` = 'idx_comment_paging'");
+
+        if (!$resultIndex) {
+            sql_query("ALTER TABLE `{$tableName}` ADD INDEX `idx_comment_paging` (`wr_parent`,`wr_comment`,`wr_comment_reply`);", true);
+            $is_check = true;
+        }
+    }
+    return $is_check;
+});
