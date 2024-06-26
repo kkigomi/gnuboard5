7633b132fbff84f96a40de2068ce0fd40ad53d57
zipuragi <zipuragi@gmail.com>
2024-06-26 21:40:16 +0900

    글조회 서명 날짜 표시 형식 변경

--- theme/damoang/layout/basic/widget/mb-sign/widget.php
+++ theme/damoang/layout/basic/widget/mb-sign/widget.php
@@ -120,7 +120,7 @@ $sign_list_cnt = count($sign_list);
                     </div>
                     <div class="f-sm fw-normal ms-md-2">
                         <span class="sr-only">등록일</span>
-                        <?php echo na_date($sign_list[$i]['wr_datetime'], 'orangered', 'H:i', 'm.d', 'Y.m.d') ?>
+                        <?php echo na_date($sign_list[$i]['wr_datetime'], 'orangered') ?>
                     </div>
                 </li>
             <?php } ?>
