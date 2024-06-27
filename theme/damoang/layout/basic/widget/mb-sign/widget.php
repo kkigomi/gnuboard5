4cc8851915729c44dedae7d3b6ba15e1a99c1c88
zipuragi <zipuragi@gmail.com>
2024-06-27 16:14:35 +0900

    서명영역 UI 개선

--- theme/damoang/layout/basic/widget/mb-sign/widget.php
+++ theme/damoang/layout/basic/widget/mb-sign/widget.php
@@ -33,6 +33,19 @@ $sign_list = array();
 $sign_list = na_board_rows($wset);
 $sign_list_cnt = count($sign_list);
 
+/*
+서명 디자인 옵션
+    1 : 서명이 프로필 오른쪽 영역에 표시
+    2 : 서명이 프로필 아래 영역에 단독으로 표시
+*/
+$mb_sign_ui = '1';
+
+/*
+ 배너 옵션(LONG, SHORT)
+    LONG : 긴 배너, 설명 없이 긴 이미지배너로 구성된다.
+    SHORT : 짧은 배너(배너와 설명으로 구성되어 진다)
+ */
+$mb_sign_banner_type = 'LONG';
 ?>
 <style>
     .float-start .sv_wrap a .profile_img {
@@ -46,13 +59,8 @@ $sign_list_cnt = count($sign_list);
 
 <!-- ================= 서명 New Start=================  -->
 <div class="border mx-3 mx-sm-0 mb-3 p-3 rounded-3">
-    <div class="row row-cols-1 row-cols-md-2 align-items-center">
+    <div class="row row-cols-1 row-cols-md-2 align-items-center" id="sign-profile-container">
         <div class="col-md-4 col-sm-5 pb-3" id="sign-profile">
-            <div class="pb-3">
-                <?php echo na_widget('damoang-image-banner', 'sign-banner'); ?>
-                신규 광고 slot 발견 ❤️❤️ 일단은 저만 쓸게요. <br>
-                비싼(프리미엄) 자리에 광고를 넣어보세요. <br>
-            </div>
             <div class="text-center mb-2 mb-sm-0">
                 <img src="<?php echo na_member_photo($mbs['mb_id']) ?>" class="rounded-circle">
             </div>
@@ -75,9 +83,11 @@ $sign_list_cnt = count($sign_list);
             <p class="mt-3"><?php echo $mb['mb_signature'] ?></p>
         </div>
     </div>
+
+
     <div class="border-top mt-3" id="sign-recent-list-container">
         <div id="sign-recent-list">
-            <ul class="list-group list-group-flush border-bottom" style="padding-left:0px;overflow-y:auto;">
+            <ul class="list-group list-group-flush border-bottom" style="padding-left:0px;overflow-y:auto;max-height:205px">
                 <?php
                 // 리스트
                 for ($i = 0; $i < $sign_list_cnt; $i++) {
@@ -133,6 +143,38 @@ $sign_list_cnt = count($sign_list);
             </ul>
         </div>
     </div>
+    <!-- 왼쪽 컨텐츠 End -->
+     <?php
+        if($mb_sign_ui == '1' ){
+     ?>
+        <div class="row row-cols-1 align-items-center pt-3 border-top" id="sign-profile-container">
+            <div id="sign-content">
+                <p class="mt-3"><?php echo $mb['mb_signature'] ?></p>
+            </div>
+        </div>
+    <?php } ?>
+    <div class="border-top"></div>
+    <?php
+        if($mb_sign_banner_type == "SHORT") {
+    ?>
+            <?php // 짧은 배너를 사용할 경우 아래에 짧은 배너 정보를 넣어주세요.?>
+            <div class="row row-cols-1 row-cols-md-2 align-items-center pt-3" id="sign-ad-container">
+                <div class="col-md-4 col-sm-5 border-end" id="sign-ad-banner">
+                    <?php echo na_widget('damoang-image-banner', 'sign-banner'); ?>
+                </div>
+                <div class="col-md-8 col-sm-7" id="sign-ad-content">
+                    신규 광고 slot 발견 ❤️❤️ 일단은 저만 쓸게요. <br>
+                    비싼(프리미엄) 자리에 광고를 넣어보세요. <br>
+                </div>
+            </div>
+    <?php } else {?>
+            <?php // 긴 배너를 사용할 경우 아래에 긴 배너 정보를 넣어주세요.?>
+            <div class="row row-cols-1 align-items-center pt-3" id="sign-profile-container">
+                <div id="sign-content">
+                    <?php echo na_widget('damoang-image-banner', 'sign-banner'); ?>
+                </div>
+            </div>
+    <?php } ?>
 </div>
 
 <?php if ($setup_href) { ?>
@@ -144,24 +186,23 @@ $sign_list_cnt = count($sign_list);
 <?php } ?>
 <script>
     var signature = `<?=$mb['mb_signature']?>`;
+    var signRecentList = $("#sign-recent-list");
+    var signContent = $("#sign-content");
+    var signRecentListContainer = $("#sign-recent-list-container");
+    var signProfile = $("#sign-profile");
 
-    // 서명이 없을 경우
-    if (signature === "" || signature === null || signature === undefined) {
-        console.debug("in");
-        $("#sign-recent-list").hide();
-        // 게시글 목록을 프로플 우측으로 추가 후 기존 것 삭제
-        var recentList = $("#sign-recent-list").html();
-        $("#sign-content").addClass("border-start");
-        $("#sign-content").html(recentList);
-        $("#sign-recent-list").remove();
+    function moveRecentListToContent() {
+        signRecentList.hide();
+        signContent.addClass("border-start").html(signRecentList.html());
+        signRecentList.remove();
+        signRecentListContainer.removeClass("border-top");
+    }
 
-        $("#sign-recent-list-container").removeClass("border-top");
-        // 프로필 영역 높이 만큼 게시글 목록 최대 높이를 늘려줌
-        var strMaxHeight = $("#sign-profile").height();
-        $("#sign-content ul").css("max-height", strMaxHeight);
-    } else { //서명 있을 경우
-        $("#sign-recent-list ul").css("max-height", "205px")
-        $("#sign-profile").addClass("border-end");
-        $("#sign-content").removeClass("border-start");
+    if (!signature || "<?=$mb_sign_ui?>" === "1") {
+        moveRecentListToContent();
+    } else {
+        signRecentList.find("ul").css("max-height", "205px");
+        signProfile.addClass("border-end");
+        signContent.removeClass("border-start");
     }
 </script>
