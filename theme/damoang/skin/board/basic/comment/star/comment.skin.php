3765cdf66dd75857a92ffada7062e66d805fe3ee
rough-life <lim7897@hotmail.com>
2024-07-07 03:25:46 +0000

    fix: 별점댓글 스킨에서 대댓글이 달릴 경우 삭제 버튼이 표시되지 않는 문제 수정

--- theme/damoang/skin/board/basic/comment/star/comment.skin.php
+++ theme/damoang/skin/board/basic/comment/star/comment.skin.php
@@ -107,6 +107,8 @@ var char_max = parseInt(<?php echo $comment_max ?>); // 최대
 
             $comment_name = get_text($list[$i]['wr_name']);
 
+            $list[$i]['is_del'] = ($is_admin == 'super') || ($list[$i]['mb_id'] == $member['mb_id']) ? true : false;
+
             // 글 작성자가 쓴 댓글, 로그인 한 사용자가 쓴 댓글, 일반 댓글 색상으로 구분하기
             if (!empty($view['mb_id']) && $view['mb_id'] == $list[$i]['mb_id']) {
                 $by_writer = 'bg-secondary-subtle'; // 글 작성자가 쓴 댓글
