415a508602c501eb28812da800e581a71db90085
KyleDEV <unilook@me.com>
2024-07-08 15:31:45 +0900

    로컬DB설정:더미회원 추가, 게시판 글쓰기에디터 기본설정. 위젯: 직홍게회원 추가

---
+++ .docker/initfile/data/nariya/widget/w-promotion-ad-insertion-pai-mo.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$data=array (
+  'd' => 
+  array (
+    'advertisers' => 
+    array (
+      0 => '홍보맨11',
+      1 => '홍보맨12',
+      2 => '홍보맨13',
+      3 => '홍보맨14',
+      4 => '홍보맨15',
+    ),
+    'board_exception' => 'promotion,truthroom',
+    'insert_index' => '0',
+    'min_cnt_for_insert_index' => '5',
+    'how_many_to_display' => '2',
+  ),
+);
